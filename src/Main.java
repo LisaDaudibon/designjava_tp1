2bac0e07d5ca0f0b65a71dc49f32a571e338be5a
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-12 14:54:40 +0100

    :sparkles: LD adding a fibonacci method

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,24 @@ public class Main {
         return sum;
     }
 
+    public static int fibonacci(int n) {
+        {
+            // fibonacci(0) = 0
+            // fibonacci(1) = 1
+            if (n < 2) {
+                return (n);
+            }
+
+            return fibonacci(n - 2) + fibonacci(n - 1);
+        }
+
+    }
+
     public static void main(String[] args) {
         int result = pair();
-        System.out.printf("Le résultat de la somme des pairs entre %s et %s inclus est de %s", 0, 100, result);
+        System.out.printf("Le résultat de la somme des pairs entre %s et %s inclus est de %s \n", 0, 100, result);
+
+        int suiteF = fibonacci(10);
+        System.out.print(suiteF);
     }
 }
